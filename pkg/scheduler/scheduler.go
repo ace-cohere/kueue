987238df23c7a7d34e82fffc989068071bc6e74e
Ace Eldeib <ace@cohere.com>
2025-04-10 16:57:56 -0400

    fix: mutate base object directly instead of double patch

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -594,16 +594,10 @@ func (s *Scheduler) requeueAndUpdate(ctx context.Context, e entry) {
 	log.V(2).Info("Workload re-queued", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "requeueReason", e.requeueReason, "added", added, "status", e.status)
 
 	if e.status == notNominated || e.status == skipped {
-		log.V(3).Info("Base object pre patch", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "obj", fmt.Sprintf("%#+v\n", e.Obj))
-		patch := workload.BaseSSAWorkload(e.Obj)
-		log.V(3).Info("Base SSA patch", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "patch", fmt.Sprintf("%#+v\n", patch))
-		workload.AdmissionStatusPatch(e.Obj, patch, true)
-		log.V(3).Info("Workload admission status patch", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "patch", fmt.Sprintf("%#+v\n", patch))
-		reservationIsChanged := workload.UnsetQuotaReservationWithCondition(patch, "Pending", e.inadmissibleMsg, s.clock.Now())
-		resourceRequestsIsChanged := workload.PropagateResourceRequests(patch, &e.Info)
+		reservationIsChanged := workload.UnsetQuotaReservationWithCondition(e.Obj, "Pending", e.inadmissibleMsg, s.clock.Now())
+		resourceRequestsIsChanged := workload.PropagateResourceRequests(e.Obj, &e.Info)
 		if reservationIsChanged || resourceRequestsIsChanged {
-			log.V(3).Info("Patching workload status", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "patch", fmt.Sprintf("%#+v\n", patch))
-			if err := workload.ApplyAdmissionStatus(ctx, s.client, patch, true); err != nil {
+			if err := workload.ApplyAdmissionStatus(ctx, s.client, e.Obj, true); err != nil {
 				log.Error(err, "Could not update Workload status")
 			}
 		}
